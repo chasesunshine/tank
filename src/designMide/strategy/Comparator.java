5d5829a6eb6378fee13eaeaf18b18725e3b88269
chasesunshine <1061902084@qq.com>
2023-07-27 17:02:00 +0800

    策略模式 - 封装的是做一件事情的时候  不同的执行方式

--- src/designMide/strategy/Comparator.java
+++ src/designMide/strategy/Comparator.java
@@ -1,5 +1,21 @@
 package designMide.strategy;
 
+// @FunctionalInterface 这个注解表示这是一个函数式接口
+@FunctionalInterface
 public interface Comparator<T> {
     int compare(T o1,T o2);
+
+    /**
+     * 为了解决该问题，JDK1.8中引入了一种新的机制：接口可以支持在声明方法的同时，提供实现。
+     * 用到的方式就是在接口中定义默认方法和静态方法。
+     * 实现含有默认方法或者静态方法的接口的类可以继承这些方法并直接使用，不再需要强制在实现类中去实现（重写）接口中的方法了。
+     *
+     * https://blog.csdn.net/lalala_dxf/article/details/123216972
+     */
+    default void m(){
+        System.out.printf("m");
+    }
+    public static void test(){
+        System.out.println("我是StaticMethod接口中的静态方法！");
+    }
 }
--- src/designMide/strategy/Main.java
+++ src/designMide/strategy/Main.java
@@ -7,14 +7,22 @@ public class Main {
 //        int [] a = {9,2,3,5,7,1,4};
 
         Cat[] a = {new Cat(3,3),new Cat(1,1),new Cat(5,5)};
-        CatWeightComparator comparator = new CatWeightComparator();
+//        CatWeightComparator comparator = new CatWeightComparator();
         Sorter<Cat> sorter = new Sorter<>();
 
 //        Dog[] a = {new Dog(3),new Dog(1),new Dog(5)};
 //        DogComparator comparator = new DogComparator();
 //        Sorter<Dog> sorter = new Sorter<>();
 
-        sorter.sort(a,comparator);
+        sorter.sort(a,(o1,o2)->{
+            if(o1.weight < o2.weight){
+                return -1;
+            } else if (o1.weight < o2.weight) {
+                return 1;
+            }else {
+                return 0;
+            }
+        });
         System.out.printf(Arrays.toString(a));
     }
 }
