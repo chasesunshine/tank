37f85bc68d6d78430412e75416804482fd2fac75
马佳健 <majiajian@dongfangfuli.com>
2023-08-06 22:45:39 +0800

    装饰器模式

--- src/com/mashibing/tank/strategy/DefaultFireStrategy.java
+++ src/com/mashibing/tank/strategy/DefaultFireStrategy.java
@@ -10,11 +10,12 @@ public class DefaultFireStrategy implements FireStrategy{
         int bX = t.x + Tank.WIDTH/2 - Bullet.WIDTH/2;
         int bY = t.y + Tank.HEIGHT/2 - Bullet.HEIGHT/2;
 
-        GameModel.getInstance().add(new RectDecorator(
-                new TailDecorator(
-                        new Bullet(bX,bY,t.dir,t.group)
-                )
-        ));
+//        GameModel.getInstance().add(new RectDecorator(
+//                new TailDecorator(
+//                        new Bullet(bX,bY,t.dir,t.group)
+//                )
+//        ));
+        new Bullet(bX,bY,t.dir,t.group);
 
         if(t.group == Group.GOOD){
             new Thread(()->new Audio("audio/tank_fire.wav").play()).start();
