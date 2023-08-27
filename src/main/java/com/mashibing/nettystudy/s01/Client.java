c7148917b038ad9d5757692976242a20c7259c01
马佳健 <majiajian@dongfangfuli.com>
2023-08-27 21:38:32 +0800

    netty - 客户端和服务端

--- src/main/java/com/mashibing/nettystudy/s01/Client.java
+++ src/main/java/com/mashibing/nettystudy/s01/Client.java
@@ -1,17 +1,17 @@
 package com.mashibing.nettystudy.s01;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoopGroup;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.ReferenceCountUtil;
 
 public class Client {
     public static void main(String[] args) throws Exception{
-        EventLoopGroup group = new NioEventLoopGroup();
+        EventLoopGroup group = new NioEventLoopGroup(1);
 
         Bootstrap b = new Bootstrap();
 
@@ -37,6 +37,9 @@ public class Client {
             f.sync();
 
             System.out.println("...");
+
+            // 不加这句话 client就结束了，客户端关闭掉了，它们之间的channel断了
+            f.channel().closeFuture().sync();
         } finally {
             group.shutdownGracefully();
         }
@@ -51,6 +54,33 @@ class ClientChannelInitializer extends ChannelInitializer<SocketChannel>{
         System.out.println(ch + "只是再通道初始化的时候调用");
 
         // 这里还有一个监听 observer 套 observer
-//        ch.pipeline().addLast();
+        ch.pipeline().addLast(new ClientHandler());
+    }
+}
+class ClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = null;
+        try {
+            buf = (ByteBuf) msg;
+            byte[] bytes = new byte[buf.readableBytes()];
+            buf.getBytes(buf.readerIndex(),bytes);
+            System.out.println(new String(bytes));
+
+//            System.out.println(buf);
+//            System.out.println(buf.refCnt());
+        }finally {
+            if(buf != null){
+                ReferenceCountUtil.release(buf);
+//                System.out.println(buf.refCnt());
+            }
+        }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        // channel第一次连上可用，写出一个字符串
+        ByteBuf buf = Unpooled.copiedBuffer("hello".getBytes());
+        ctx.writeAndFlush(buf);
     }
 }
--- src/main/java/com/mashibing/nettystudy/s01/Server.java
+++ src/main/java/com/mashibing/nettystudy/s01/Server.java
@@ -1,16 +1,91 @@
 package com.mashibing.nettystudy.s01;
 
+import com.sun.media.sound.ReferenceCountingDevice;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        ServerSocket ss = new ServerSocket();
-        ss.bind(new InetSocketAddress(8888));
-        Socket s = ss.accept();
+    public static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
-        System.out.println("a client connect");
+    public static void main(String[] args){
+        // bossGroup只负责连接
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(2);
+
+        ServerBootstrap b = new ServerBootstrap();
+        try {
+            ChannelFuture f = b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new ServerChildHandler());
+                        }
+                    })
+                    .bind(8888)
+                    //因为netty是异步的，所以要等.bind(8888) 成功了之后 才会同步执行 .sync()
+                    .sync();
+
+            System.out.println("server start");
+
+            // 起到阻塞作用
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
+
+class ServerChildHandler extends ChannelInboundHandlerAdapter{
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Server.clients.add(ctx.channel());
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = null;
+        try {
+            buf = (ByteBuf) msg;
+
+
+            byte[] bytes = new byte[buf.readableBytes()];
+            buf.getBytes(buf.readerIndex(),bytes);
+            System.out.println(new String(bytes));
+
+            // ctx.writeAndFlush(msg);这个是自动释放
+            Server.clients.writeAndFlush(msg);
+//            System.out.println(buf);
+//            System.out.println(buf.refCnt());
+        }finally {
+//            if(buf != null){
+//                ReferenceCountUtil.release(buf);
+////                System.out.println(buf.refCnt());
+//            }
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
     }
 }
