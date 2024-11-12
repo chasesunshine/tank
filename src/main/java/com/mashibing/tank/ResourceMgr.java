b15f6a86d517fa202ae0d1aaead805af2d20a6f1
ma.jiajian <1061902084@qq.com>
2024-11-12 14:53:44 +0800

    commit

--- src/main/java/com/mashibing/tank/ResourceMgr.java
+++ src/main/java/com/mashibing/tank/ResourceMgr.java
@@ -16,7 +16,7 @@ public class ResourceMgr {
 			goodTankR = ImageUtil.rotateImage(goodTankU, 90);
 			goodTankD = ImageUtil.rotateImage(goodTankU, 180);
 
-			badTankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/badTank1.png"));
+			badTankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/BadTank1.png"));
 			badTankL = ImageUtil.rotateImage(badTankU, -90);
 			badTankR = ImageUtil.rotateImage(badTankU, 90);
 			badTankD = ImageUtil.rotateImage(badTankU, 180);
